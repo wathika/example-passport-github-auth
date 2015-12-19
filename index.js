e48fb77fc27073725938afd71d5a54251464c53d
wathika <vcwathika@gmail.com>
2015-12-18 17:20:54 -0800

    initial index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Module dependencies
+ */
+
+var express = require('express')
+var app = express()
+
+app.get('/', function(req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000, function() {
+  console.log('listen on *:3000')
+})
