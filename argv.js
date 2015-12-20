2c40ab0bb36704c3a9bf22b72001fd3e528af66c
wathika <vcwathika@gmail.com>
2015-12-20 13:38:47 -0800

    fix typo

--- argv.js
+++ argv.js
@@ -2,7 +2,7 @@
 
 var argv = require('cmdenv')('github-auth')
   .option('--github-client-id <value>', 'Your github client id')
-  .options('--github-client-secret <value>', 'Your github client secret')
+  .option('--github-client-secret <value>', 'Your github client secret')
   .parse(process.argv)
 
 module.exports = {
